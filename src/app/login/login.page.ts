46f830e8eafc6fe40e89676e448dab39584f92e0
nicolas21467 <nico21467@gmail.com>
2021-11-21 03:51:41 -0300

    Update login.page.ts

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+import { ToastController } from '@ionic/angular';
+import{Router, router}from '@angular/router';
+import { totalmem } from 'os';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
-  constructor() { }
+  listado=[];
+  constructor(private api: ApiService, private ToastController:ToastController,private router:Router) { }
 
   ngOnInit() {
+    this.api.getUsers();
   }
+  async verificar(nombre:HTMLInputElement,password:HTMLInputElement){
+    let listado= this.api.listado;
+    let usuario = nombre.value;
+    let clave =password.value;
+    let validar =false;
 
+    if (usuario.trim().length ==3 && clave.trim().length ==3){
+    const toast = await this.ToastController.create({
+      message:'los valores ingresados no son validos',
+      duration: 3000,
+      color:'primary'
+
+    });
+    toast.present();
+    return toast;
+  }
+  else if(usuario.trim().length == 0){
+    const toast = await this.ToastController.create({
+      message:'porfavor rellene el usuario',
+      duration: 3000,
+      color:'primary'
+
+    });
+    toast.present();
+    return toast;
+  }
+  else if(clave.trim().length == 0){
+    const toast = await this.ToastController.create({
+      message:'porfavor rellene la contraseña con una valida',
+      duration: 3000,
+      color:'primary'
+
+    });
+    toast.present();
+    return toast;
+  }
+  else if(clave != "1234"){
+    const toast = await this.ToastController.create({
+      message:'la contraseña no es valida ',
+      duration: 3000,
+      color:'primary'
+
+    });
+    toast.present();
+    return toast;
+  }
 }
